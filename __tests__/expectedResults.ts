eabff3b4d336b4f61e8054287b015c1f3cead5ee
Weihao <mail@weihao.dev>
2023-07-29 15:06:22 +0200

    1. added tests

---
+++ __tests__/expectedResults.ts
@@ -0,0 +1,126 @@
+export const Example1Expected = [
+    {
+        "name": "__root",
+        "pins": {
+            "1": "net_0.5V"
+        }
+    },
+    {
+        "name": "gnd",
+        "pins": {
+            "1": "gnd"
+        }
+    },
+    {
+        "name": "V5V",
+        "pins": {
+            "1": "net_0.5V"
+        }
+    },
+    {
+        "name": "R1",
+        "pins": {
+            "1": "net_0.5V",
+            "2": "NET_1"
+        }
+    },
+    {
+        "name": "R2",
+        "pins": {
+            "1": "NET_1",
+            "2": "NET_2"
+        }
+    },
+    {
+        "name": "R3",
+        "pins": {
+            "1": "NET_2",
+            "2": "gnd"
+        }
+    },
+    {
+        "name": "res_3.COMP_1_20k",
+        "pins": {
+            "1": "net_0.5V",
+            "2": "gnd"
+        }
+    }
+];
+
+export const Example2Expected = [
+    {
+      "name": "__root",
+      "pins": {
+        "1": "NET_1"
+      }
+    },
+    {
+      "name": "gnd",
+      "pins": {
+        "1": "gnd"
+      }
+    },
+    {
+      "name": "R1",
+      "pins": {
+        "1": "NET_1",
+        "2": "NET_2"
+      }
+    },
+    {
+      "name": "R2",
+      "pins": {
+        "1": "NET_2",
+        "2": "NO_NET"
+      }
+    },
+    {
+      "name": "R3",
+      "pins": {
+        "1": "NET_2",
+        "2": "NET_3"
+      }
+    },
+    {
+      "name": "R4",
+      "pins": {
+        "1": "NET_3",
+        "2": "NET_5"
+      }
+    },
+    {
+      "name": "R5",
+      "pins": {
+        "1": "NET_2",
+        "2": "NET_4"
+      }
+    },
+    {
+      "name": "R6",
+      "pins": {
+        "1": "NET_4",
+        "2": "NO_NET"
+      }
+    },
+    {
+      "name": "R7",
+      "pins": {
+        "1": "NET_5",
+        "2": "NET_6"
+      }
+    },
+    {
+      "name": "R8",
+      "pins": {
+        "1": "NET_6",
+        "2": "gnd"
+      }
+    },
+    {
+      "name": "R9",
+      "pins": {
+        "1": "NET_6",
+        "2": "gnd"
+      }
+    }
+  ]
---
+++ __tests__/testExamples.ts
@@ -0,0 +1,53 @@
+import fs from 'fs';
+import CircuitScriptParser from './../src/antlr/CircuitScriptParser';
+import CircuitScriptLexer from './../src/antlr/CircuitScriptLexer';
+
+import { CharStream, CommonTokenStream } from 'antlr4';
+import { MainVisitor } from './../src/visitor';
+import { Example1Expected, Example2Expected } from './expectedResults';
+
+describe('test examples', () => {
+
+    test.each([
+        ['examples/example.cst', Example1Expected],
+        ['examples/example2.cst', Example2Expected]
+    ])("test %s", async (fileName, expectedResult) => {
+        const data = await getFile(fileName);
+        const result = getScriptOutput(data);
+        expect(result).toStrictEqual(expectedResult);
+    });
+
+    function getFile(filename: string): Promise<string> {
+        return new Promise(resolve => {
+            fs.readFile(filename, 'utf8', (err, data) => {
+                if (err) {
+                    throw err;
+                }
+
+                resolve(data);
+            });
+        });
+    }
+
+    function getScriptOutput(data: string): object {
+        const chars = new CharStream(data);
+        const lexer = new CircuitScriptLexer(chars);
+        const tokens = new CommonTokenStream(lexer);
+
+        const parser = new CircuitScriptParser(tokens);
+        const tree = parser.script();
+
+        const visitor = new MainVisitor();
+        visitor.silent = true;
+        visitor.getExecutor().silent = true;
+
+        try {
+            visitor.visit(tree);
+        } catch (err) {
+            console.log("got error", err);
+        }
+
+        const result = visitor.dump2();
+        return result;
+    }
+});
