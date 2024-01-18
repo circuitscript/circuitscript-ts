7220e589d4b1a8f730255758dd42ae8a7535cf55
Weihao <mail@weihao.dev>
2024-01-18 07:13:36 +0100

    1. added script to regenerate test data

---
+++ src/regenerate-tests.ts
@@ -0,0 +1,25 @@
+import fs from 'fs';
+import { renderScript } from './main';
+
+const mainDir = './__tests__/renderData/';
+
+const cstFiles = [];
+
+const files = fs.readdirSync(mainDir);
+files.forEach(file => {
+    if (file.endsWith('.cst')){
+        cstFiles.push(file);
+    }
+});
+
+const useCurrentDir = './examples/';
+
+cstFiles.forEach(file => {
+    const inputPath = mainDir + file;
+    const scriptData = fs.readFileSync(inputPath, { encoding: 'utf-8' });
+    
+    const outputPath = inputPath + '.svg';
+    renderScript(scriptData, outputPath, useCurrentDir, false, false);
+
+    console.log('generated ', inputPath);
+});
