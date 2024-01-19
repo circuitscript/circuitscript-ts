551fea9478d46d0d30407ea286300e8fb4caa933
Weihao <mail@weihao.dev>
2024-01-19 19:59:05 +0100

    1. minor refactor to tests 2. added tests for net namespace syntax 3. backup example

---
+++ __tests__/parseScripts.ts
@@ -0,0 +1,374 @@
+import { ComponentPinNet } from "../src/objects/types";
+
+
+export class ScriptTest {
+
+    script: string;
+    expected: ComponentPinNet[];
+
+    constructor(script:string, expected: ComponentPinNet[]){
+        this.script = script;
+        this.expected = expected;
+    }
+}
+
+export const script1 = new ScriptTest(`
+U1 = create component:
+    pins: 10
+
+U2 = create component:
+    pins: 10
+
+at U1: 
+    2:
+        wire right 200 
+        to U2 pin 1
+
+at U1 pin 10
+wire right 180 up 40
+
+branch:
+    wire auto
+    to U2 pin 5
+
+wire auto
+to U2 pin 3
+`, [
+    ['/NET_1', 'U1', 2],
+    ['/NET_1', 'U2', 1],
+    ['/NET_2', 'U1', 10],
+    ['/NET_2', 'U2', 5],
+    ['/NET_2', 'U2', 3]
+]);
+
+export const script2 = new ScriptTest(`
+import lib
+gnd = dgnd()
+
+# Test functions
+def power_input():
+    return create component:
+        pins: 3
+        arrange:
+            left: 1,3
+            right: 2
+
+
+J1 = power_input()
+
+at J1 pin 1
+wire left 20 down 20
+
+branch:
+    wire right 20
+    to J1 pin 3
+
+wire down 20
+to gnd
+`, [
+    [ '/GND', 'gnd', 1 ],
+    [ '/GND', 'J1', 1 ],
+    [ '/GND', 'J1', 3 ],
+    [ '/GND', 'gnd:0', 1 ]
+  ]);
+
+export const script3 = new ScriptTest(`
+import lib
+
+at label("hello")
+wire right 40 
+
+branch:
+    wire down 40
+    
+    branch:
+        wire down 20
+        add diode() ..angle = 90
+
+    wire left 20
+    add diode() pin 2
+    wire left 20 down 20
+    add diode() ..angle = 90
+
+wire right 40 
+add diode()
+`, [
+    [ '/NET_1', 'diode_1.COMP_1', 1 ],
+    [ '/NET_1', 'diode_2.COMP_1', 1 ],
+    [ '/hello', 'label_0.COMP_1_1', 1 ],
+    [ '/hello', 'label_0.COMP_1_1:0', 1 ],
+    [ '/hello', 'diode_0.COMP_1', 1 ],
+    [ '/hello', 'diode_1.COMP_1', 2 ],
+    [ '/hello', 'diode_3.COMP_1', 1 ]
+  ]);
+
+export const script6 = new ScriptTest(`
+import lib
+
+vcc = net("3v3")
+midpt = net("out")
+gnd = dgnd()
+
+at vcc
+wire down 20
+add res(20k) down
+wire down 20
+branch:
+    wire down 20
+    add res(20k) down
+    wire down 20
+    to gnd
+wire right 60
+to midpt ..angle = 90
+
+at vcc
+wire down 20
+add res(10k) down
+wire down 20 right 40 up 20
+to midpt
+`, [
+    [ '/3v3', 'vcc', 1 ],
+    [ '/3v3', 'vcc:0', 1 ],
+    [ '/3v3', 'res_0.COMP_1_20k', 1 ],
+    [ '/3v3', 'vcc:1', 1 ],
+    [ '/3v3', 'res_2.COMP_1_10k', 1 ],
+    [ '/GND', 'gnd', 1 ],
+    [ '/GND', 'gnd:0', 1 ],
+    [ '/GND', 'res_1.COMP_1_20k', 2 ],
+    [ '/out', 'midpt', 1 ],
+    [ '/out', 'res_0.COMP_1_20k', 2 ],
+    [ '/out', 'res_1.COMP_1_20k', 1 ],
+    [ '/out', 'midpt:0', 1 ],
+    [ '/out', 'midpt:1', 1 ],
+    [ '/out', 'res_2.COMP_1_10k', 2 ]
+  ]);
+
+export const script7 = new ScriptTest(`
+import lib
+
+v5v = net("5V")
+gnd = dgnd()
+
+def tmp1():
+    at v5v
+    wire down 20
+    add res(10k)
+    wire down 20
+    to gnd
+
+tmp1()
+
+tmp1()
+`, [
+    [ '/5V', 'v5v', 1 ],
+    [ '/5V', 'tmp1_0.v5v:0', 1 ],
+    [ '/5V', 'tmp1_0.res_0.COMP_1_10k', 1 ],
+    [ '/5V', 'tmp1_1.v5v:0', 1 ],
+    [ '/5V', 'tmp1_1.res_1.COMP_1_10k', 1 ],
+    [ '/GND', 'gnd', 1 ],
+    [ '/GND', 'tmp1_0.gnd:0', 1 ],
+    [ '/GND', 'tmp1_0.res_0.COMP_1_10k', 2 ],
+    [ '/GND', 'tmp1_1.gnd:0', 1 ],
+    [ '/GND', 'tmp1_1.res_1.COMP_1_10k', 2 ]
+  ]);
+
+export  const script8 = new ScriptTest(`
+import lib
+
+v5v = supply("5v")
+gnd = dgnd()
+
+def divider(power_net, r1, r2, output_net):
+    at power_net
+    wire down 20
+    add res(r1) down
+    wire down 20
+
+    branch:
+        wire right 20
+        add output_net
+        wire right 20
+
+    wire down 20
+    add res(r2) down
+    wire down 20
+    to gnd
+
+divider(v5v, 10k, 20k, label("hello"))
+`,  [
+    [ '/5v', 'v5v', 1 ],
+    [ '/5v', 'divider_0.v5v:0', 1 ],
+    [ '/5v', 'divider_0.res_0.COMP_1_10k', 1 ],
+    [ '/GND', 'gnd', 1 ],
+    [ '/GND', 'divider_0.gnd:0', 1 ],
+    [ '/GND', 'divider_0.res_1.COMP_1_20k', 2 ],
+    [ '/hello', 'label_0.COMP_1_1', 1 ],
+    [ '/hello', 'divider_0.res_0.COMP_1_10k', 2 ],
+    [ '/hello', 'divider_0.res_1.COMP_1_20k', 1 ]
+  ]);
+
+export const script9 = new ScriptTest(`
+import lib
+
+v5v = supply("5v")
+gnd = dgnd()
+
+net("global_name")
+
+def divider(power_net, r1, r2, net_name):
+    frame:
+        ..border = 0
+        
+        at power_net
+        wire down 20
+        add res(r1) down
+        wire down 20
+
+        branch:
+            wire right 20
+            add label("inner_name")
+            wire right 60
+            add res(100k) right
+            wire right 20
+            add / label("global_name")
+            wire right 40
+
+            add res(0)
+
+            add / label("global_name_2")
+
+        wire down 20
+        add res(r2) down
+        wire down 20
+        to gnd
+
+frame:
+    ..border = 0
+    ..direction = "row"
+
+    /div1 divider(v5v, 10k, 20k, "hello2")
+    /div2 divider(v5v, 10k, 20k, "hello1")
+`, [
+    [ '/5v', 'v5v', 1 ],
+    [ '/5v', 'divider_0.v5v:0', 1 ],
+    [ '/5v', 'divider_0.res_0.COMP_1_10k', 1 ],
+    [ '/5v', 'divider_1.v5v:0', 1 ],
+    [ '/5v', 'divider_1.res_4.COMP_1_10k', 1 ],
+    [ '/GND', 'gnd', 1 ],
+    [ '/GND', 'divider_0.gnd:0', 1 ],
+    [ '/GND', 'divider_0.res_3.COMP_1_20k', 2 ],
+    [ '/GND', 'divider_1.gnd:0', 1 ],
+    [ '/GND', 'divider_1.res_7.COMP_1_20k', 2 ],
+    [ '/div1/inner_name', 'divider_0.label_0.COMP_1_1', 1 ],
+    [ '/div1/inner_name', 'divider_0.res_0.COMP_1_10k', 2 ],
+    [ '/div1/inner_name', 'divider_0.res_1.COMP_1_100k', 1 ],
+    [ '/div1/inner_name', 'divider_0.res_3.COMP_1_20k', 1 ],
+    [ '/div2/inner_name', 'divider_1.label_3.COMP_1_1', 1 ],
+    [ '/div2/inner_name', 'divider_1.res_4.COMP_1_10k', 2 ],
+    [ '/div2/inner_name', 'divider_1.res_5.COMP_1_100k', 1 ],
+    [ '/div2/inner_name', 'divider_1.res_7.COMP_1_20k', 1 ],
+    [ '/global_name', 'net_0.COMP_1_1', 1 ],
+    [ '/global_name', 'divider_0.label_1.COMP_1_1', 1 ],
+    [ '/global_name', 'divider_0.res_1.COMP_1_100k', 2 ],
+    [ '/global_name', 'divider_0.res_2.COMP_1_0', 1 ],
+    [ '/global_name', 'divider_1.label_4.COMP_1_1', 1 ],
+    [ '/global_name', 'divider_1.res_5.COMP_1_100k', 2 ],
+    [ '/global_name', 'divider_1.res_6.COMP_1_0', 1 ],
+    [ '/global_name_2', 'divider_0.label_2.COMP_1_1', 1 ],
+    [ '/global_name_2', 'divider_0.res_2.COMP_1_0', 2 ],
+    [ '/global_name_2', 'divider_1.label_5.COMP_1_1', 1 ],
+    [ '/global_name_2', 'divider_1.res_6.COMP_1_0', 2 ]
+  ]);
+
+export const script10 = new ScriptTest(`
+import lib
+gnd = dgnd()
+
+at v5v = supply("5V")
+wire down 20
+add R1 = res(10k) down
+wire down 20
+add R2 = res(10k) down
+wire down 20
+to C1 = cap(10n) down
+
+at C1 pin 2
+wire down 20
+to gnd
+`, [
+    ['/5V', 'v5v', 1],
+    ['/5V', 'v5v:0', 1],
+    ['/5V', 'R1', 1],
+    ['/GND', 'gnd', 1],
+    ['/GND', 'gnd:0', 1],
+    ['/GND', 'C1', 2],
+    ['/NET_1', 'R1', 2],
+    ['/NET_1', 'R2', 1],
+    ['/NET_2', 'R2', 2],
+    ['/NET_2', 'C1', 1]
+]);
+
+export const script11 = new ScriptTest(`
+import lib
+
+v5v = supply("5V")
+
+def tmp1():
+    wire right 20
+    add res(0)
+    wire right 20
+    add label("label_1")
+
+    wire right 40
+    add res(0)
+    add / label("global_label")
+
+    wire right 80
+    add res(0)
+    
+    +/inner tmp2()
+
+def tmp2():
+    wire right 20
+    add res(0)
+    add label("label_2")
+    wire right 40
+
+at v5v
+wire down 20
+branch:
+    /scope1 tmp1()
+
+wire down 40
+branch:
+    /scope2 tmp1()
+`, [
+    ['/5V', 'v5v', 1],
+    ['/5V', 'v5v:0', 1],
+    ['/5V', 'tmp1_0.__root', 1],
+    ['/5V', 'tmp1_0.res_0.COMP_1_0', 1],
+    ['/5V', 'tmp1_1.__root', 1],
+    ['/5V', 'tmp1_1.res_4.COMP_1_0', 1],
+    ['/global_label', 'tmp1_0.label_1.COMP_1_1', 1],
+    ['/global_label', 'tmp1_0.res_1.COMP_1_0', 2],
+    ['/global_label', 'tmp1_0.res_2.COMP_1_0', 1],
+    ['/global_label', 'tmp1_1.label_4.COMP_1_1', 1],
+    ['/global_label', 'tmp1_1.res_5.COMP_1_0', 2],
+    ['/global_label', 'tmp1_1.res_6.COMP_1_0', 1],
+    ['/scope1/inner/NET_1', 'tmp2_0.__root', 1],
+    ['/scope1/inner/NET_1', 'tmp1_0.tmp2_0.res_3.COMP_1_0', 1],
+    ['/scope1/inner/NET_1', 'tmp1_0.res_2.COMP_1_0', 2],
+    ['/scope1/inner/label_2', 'tmp1_0.tmp2_0.label_2.COMP_1_1', 1],
+    ['/scope1/inner/label_2', 'tmp1_0.tmp2_0.res_3.COMP_1_0', 2],
+    ['/scope1/label_1', 'tmp1_0.label_0.COMP_1_1', 1],
+    ['/scope1/label_1', 'tmp1_0.res_0.COMP_1_0', 2],
+    ['/scope1/label_1', 'tmp1_0.res_1.COMP_1_0', 1],
+    ['/scope2/inner/NET_1', 'tmp2_1.__root', 1],
+    ['/scope2/inner/NET_1', 'tmp1_1.tmp2_1.res_7.COMP_1_0', 1],
+    ['/scope2/inner/NET_1', 'tmp1_1.res_6.COMP_1_0', 2],
+    ['/scope2/inner/label_2', 'tmp1_1.tmp2_1.label_5.COMP_1_1', 1],
+    ['/scope2/inner/label_2', 'tmp1_1.tmp2_1.res_7.COMP_1_0', 2],
+    ['/scope2/label_1', 'tmp1_1.label_3.COMP_1_1', 1],
+    ['/scope2/label_1', 'tmp1_1.res_4.COMP_1_0', 2],
+    ['/scope2/label_1', 'tmp1_1.res_5.COMP_1_0', 1]
+]);
--- __tests__/testParse.ts
+++ __tests__/testParse.ts
@@ -1,34 +1,62 @@
 
-import { ComponentPinNet } from '../src/objects/types';
 import { findItem, runScript } from './helpers';
+import { script1, script10, script11, script2, script3, script6, script7, script8, script9 } from './parseScripts';
 
 describe('test parsing', () => {
 
     test.each([
-        [0, "create component command"],
-        [1, "function to create component and branching"],
-        [2, "nested branching, add with pin selected"],
-        [3, "'at' and 'to' commands will clone net components"],
-        [4, "resolve instances in upper contexts"],
-        [5, "components in function parameters"],
-        [6, "resolve nets in local and upper contexts"],
-        [7, "assignment in at/to/add statement"]
+        ["create component command", script1],
+        ["function to create component and branching", script2],
+        ["nested branching, add with pin selected", script3],
+        ["'at' and 'to' commands will clone net components", script6],
+        ["resolve instances in upper contexts", script7],
+        ["components in function parameters", script8],
+        ["resolve nets in local and upper contexts", script9],
+        ["assignment in at/to/add statement", script10],
+        ["net namespace local and global", script11],
         
-    ])('parse script - %d: %s', async (index, description) => {
+    ])('parse script - %s', async (description, scriptTest) => {
         // Test only parsing, does not check the correctness of the 
         // parsed result!
 
         // Access scripts like this to prevent having to 
         // define the script content before the test itself
-        const [script, expected] = allScripts[index];
-        const {hasError, componentPinNets} = await runScript(script);
+        const {hasError, componentPinNets} = await runScript(scriptTest.script);
 
         expect(hasError).toBe(false);
-        expect(componentPinNets).toStrictEqual(expected);
+        expect(componentPinNets).toStrictEqual(scriptTest.expected);
     });
 
     test('component annotation', async () => {
-        const { hasError, visitor } = await runScript(script4);
+        const { hasError, visitor } = await runScript(`
+import lib
+
+variant = "MainVariantX"
+gnd = dgnd()
+
+at net("5V")
+wire down 20
+branch:
+    wire down 20
+    add res(10k) ..angle = 90
+    ..place = (variant == "MainVariant")
+    wire down 20 to gnd
+
+wire right 40
+branch:
+    wire down 20
+    add res(20k) ..angle = 90
+    ..place = true
+    wire down 20
+    to gnd
+
+wire right 40
+branch:
+    wire down 20
+    add cap(100n)
+    wire down 20
+    to gnd
+`);
         expect(hasError).toBe(false);
 
         visitor.annotateComponents();
@@ -155,391 +183,6 @@ R1.mpn = "res-12345"
         expect(item1.parameters.get('mpn')).toBe('res-12345');
     });
 });
-
-
-const script1 = `
-U1 = create component:
-    pins: 10
-
-U2 = create component:
-    pins: 10
-
-at U1: 
-    2:
-        wire right 200 
-        to U2 pin 1
-
-at U1 pin 10
-wire right 180 up 40
-
-branch:
-    wire auto
-    to U2 pin 5
-
-wire auto
-to U2 pin 3
-`;
-
-const script1Expected: ComponentPinNet[] = [
-    ['__.NET_1', 'U1', 2],
-    ['__.NET_1', 'U2', 1],
-    ['__.NET_2', 'U1', 10],
-    ['__.NET_2', 'U2', 5],
-    ['__.NET_2', 'U2', 3]
-];
-
-const script2 = `
-import lib
-gnd = dgnd()
-
-# Test functions
-def power_input():
-    return create component:
-        pins: 3
-        arrange:
-            left: 1,3
-            right: 2
-
-
-J1 = power_input()
-
-at J1 pin 1
-wire left 20 down 20
-
-branch:
-    wire right 20
-    to J1 pin 3
-
-wire down 20
-to gnd
-`;
-
-const script2Expected: ComponentPinNet[] = [
-    ['__.dgnd_0.GND', 'gnd', 1],
-    ['__.dgnd_0.GND', 'J1', 1],
-    ['__.dgnd_0.GND', 'J1', 3],
-    ['__.dgnd_0.GND', 'gnd:0', 1]
-];
-
-const script3 = `
-def diode():
-    return create component:
-        pins: 2
-
-def label(value):
-    return create component:
-        pins: 1
-        display: "label"
-        params:
-            value: value
-
-at label("hello")
-wire right 40 
-
-branch:
-    wire down 40
-    
-    branch:
-        wire down 20
-        add diode() ..angle = 90
-
-    wire left 20
-    add diode() pin 2
-    wire left 20 down 20
-    add diode() ..angle = 90
-
-wire right 40 
-add diode()
-`;
-const script3Expected: ComponentPinNet[] = [
-    [ '__.NET_1', 'label_0.COMP_1_hello', 1 ],
-    [ '__.NET_1', 'diode_0.COMP_1', 1 ],
-    [ '__.NET_1', 'diode_1.COMP_1', 2 ],
-    [ '__.NET_1', 'diode_3.COMP_1', 1 ],
-    [ '__.NET_2', 'diode_1.COMP_1', 1 ],
-    [ '__.NET_2', 'diode_2.COMP_1', 1 ],
-  ];
-
-const script6 = `
-import lib
-
-vcc = net("3v3")
-midpt = net("out")
-gnd = dgnd()
-
-at vcc
-wire down 20
-add res(20k) down
-wire down 20
-branch:
-    wire down 20
-    add res(20k) down
-    wire down 20
-    to gnd
-wire right 60
-to midpt ..angle = 90
-
-at vcc
-wire down 20
-add res(10k) down
-wire down 20 right 40 up 20
-to midpt
-`;
-
-const script6Expected: ComponentPinNet[] = [
-    ['__.dgnd_0.GND', 'gnd', 1],
-    ['__.dgnd_0.GND', 'gnd:0', 1],
-    ['__.dgnd_0.GND', 'res_1.COMP_1_20k', 2],
-    ['__.net_0.3v3', 'vcc', 1],
-    ['__.net_0.3v3', 'vcc:0', 1],
-    ['__.net_0.3v3', 'res_0.COMP_1_20k', 1],
-    ['__.net_0.3v3', 'vcc:1', 1],
-    ['__.net_0.3v3', 'res_2.COMP_1_10k', 1],
-    ['__.net_1.out', 'midpt', 1],
-    ['__.net_1.out', 'res_0.COMP_1_20k', 2],
-    ['__.net_1.out', 'res_1.COMP_1_20k', 1],
-    ['__.net_1.out', 'midpt:0', 1],
-    ['__.net_1.out', 'midpt:1', 1],
-    ['__.net_1.out', 'res_2.COMP_1_10k', 2]
-];
-
-const script7 = `
-import lib
-
-v5v = net("5V")
-gnd = dgnd()
-
-def tmp1():
-    at v5v
-    wire down 20
-    add res(10k)
-    wire down 20
-    to gnd
-
-tmp1()
-
-tmp1()`
-
-const script7Expected: ComponentPinNet[] = [
-    ['__.dgnd_0.GND', 'gnd', 1],
-    ['__.dgnd_0.GND', 'tmp1_0.gnd:0', 1],
-    ['__.dgnd_0.GND', 'tmp1_0.res_0.COMP_1_10k', 2],
-    ['__.dgnd_0.GND', 'tmp1_1.gnd:0', 1],
-    ['__.dgnd_0.GND', 'tmp1_1.res_1.COMP_1_10k', 2],
-    ['__.net_0.5V', 'v5v', 1],
-    ['__.net_0.5V', 'tmp1_0.v5v:0', 1],
-    ['__.net_0.5V', 'tmp1_0.res_0.COMP_1_10k', 1],
-    ['__.net_0.5V', 'tmp1_1.v5v:0', 1],
-    ['__.net_0.5V', 'tmp1_1.res_1.COMP_1_10k', 1]
-];
-
-const script4 = `
-import lib
-
-variant = "MainVariantX"
-gnd = dgnd()
-
-at net("5V")
-wire down 20
-branch:
-    wire down 20
-    add res(10k) ..angle = 90
-    ..place = (variant == "MainVariant")
-    wire down 20 to gnd
-
-wire right 40
-branch:
-    wire down 20
-    add res(20k) ..angle = 90
-    ..place = true
-    wire down 20
-    to gnd
-
-wire right 40
-branch:
-    wire down 20
-    add cap(100n)
-    wire down 20
-    to gnd
-
-`;
-
-const script8 = `
-import lib
-
-v5v = supply("5v")
-gnd = dgnd()
-
-def divider(power_net, r1, r2, output_net):
-    at power_net
-    wire down 20
-    add res(r1) down
-    wire down 20
-
-    branch:
-        wire right 20
-        add output_net
-        wire right 20
-
-    wire down 20
-    add res(r2) down
-    wire down 20
-    to gnd
-
-divider(v5v, 10k, 20k, label("hello"))
-`
-
-const script8Expected: ComponentPinNet[] = [
-    ['__.dgnd_0.GND', 'gnd', 1],
-    ['__.dgnd_0.GND', 'divider_0.gnd:0', 1],
-    ['__.dgnd_0.GND', 'divider_0.res_1.COMP_1_20k', 2],
-    ['__.label_0.hello', 'label_0.COMP_1_1', 1],
-    ['__.label_0.hello', 'divider_0.res_0.COMP_1_10k', 2],
-    ['__.label_0.hello', 'divider_0.res_1.COMP_1_20k', 1],
-    ['__.supply_0.5v', 'v5v', 1],
-    ['__.supply_0.5v', 'divider_0.v5v:0', 1],
-    ['__.supply_0.5v', 'divider_0.res_0.COMP_1_10k', 1]
-];
-
-const script9 = `
-import lib
-
-v5v = supply("5v")
-gnd = dgnd()
-
-net("global_name")
-
-def divider(power_net, r1, r2, net_name):
-    frame:
-        ..border = 0
-        
-        at power_net
-        wire down 20
-        add res(r1) down
-        wire down 20
-
-        branch:
-            wire right 20
-            add label("inner_name")
-            wire right 60
-            add res(100k) right
-            wire right 20
-            add label("global_name")
-            wire right 40
-
-        wire down 20
-        add res(r2) down
-        wire down 20
-        to gnd
-
-frame:
-    ..border = 0
-    ..direction = "row"
-
-    divider(v5v, 10k, 20k, "hello2")
-    divider(v5v, 10k, 20k, "hello1")
-`;
-
-const script9Expected: ComponentPinNet[] = [
-    ['__.dgnd_0.GND', 'gnd', 1],
-    ['__.dgnd_0.GND', 'divider_0.gnd:0', 1],
-    ['__.dgnd_0.GND', 'divider_0.res_2.COMP_1_20k', 2],
-    ['__.dgnd_0.GND', 'divider_1.gnd:0', 1],
-    ['__.dgnd_0.GND', 'divider_1.res_5.COMP_1_20k', 2],
-    [
-        '__.divider_0.label_0.inner_name',
-        'divider_0.label_0.COMP_1_1',
-        1
-    ],
-    [
-        '__.divider_0.label_0.inner_name',
-        'divider_0.res_0.COMP_1_10k',
-        2
-    ],
-    [
-        '__.divider_0.label_0.inner_name',
-        'divider_0.res_1.COMP_1_100k',
-        1
-    ],
-    [
-        '__.divider_0.label_0.inner_name',
-        'divider_0.res_2.COMP_1_20k',
-        1
-    ],
-    [
-        '__.divider_1.label_2.inner_name',
-        'divider_1.label_2.COMP_1_1',
-        1
-    ],
-    [
-        '__.divider_1.label_2.inner_name',
-        'divider_1.res_3.COMP_1_10k',
-        2
-    ],
-    [
-        '__.divider_1.label_2.inner_name',
-        'divider_1.res_4.COMP_1_100k',
-        1
-    ],
-    [
-        '__.divider_1.label_2.inner_name',
-        'divider_1.res_5.COMP_1_20k',
-        1
-    ],
-    ['__.net_0.global_name', 'net_0.COMP_1_1', 1],
-    ['__.net_0.global_name', 'divider_0.label_1.COMP_1_1', 1],
-    ['__.net_0.global_name', 'divider_0.res_1.COMP_1_100k', 2],
-    ['__.net_0.global_name', 'divider_1.label_3.COMP_1_1', 1],
-    ['__.net_0.global_name', 'divider_1.res_4.COMP_1_100k', 2],
-    ['__.supply_0.5v', 'v5v', 1],
-    ['__.supply_0.5v', 'divider_0.v5v:0', 1],
-    ['__.supply_0.5v', 'divider_0.res_0.COMP_1_10k', 1],
-    ['__.supply_0.5v', 'divider_1.v5v:0', 1],
-    ['__.supply_0.5v', 'divider_1.res_3.COMP_1_10k', 1]
-];
-
-const script10 = `
-import lib
-gnd = dgnd()
-
-at v5v = supply("5V")
-wire down 20
-add R1 = res(10k) down
-wire down 20
-add R2 = res(10k) down
-wire down 20
-to C1 = cap(10n) down
-
-at C1 pin 2
-wire down 20
-to gnd
-`
-
-const script10Expected: ComponentPinNet[] =[
-    [ '__.NET_1', 'R1', 2 ],
-    [ '__.NET_1', 'R2', 1 ],
-    [ '__.NET_2', 'R2', 2 ],
-    [ '__.NET_2', 'C1', 1 ],
-    [ '__.dgnd_0.GND', 'gnd', 1 ],
-    [ '__.dgnd_0.GND', 'gnd:0', 1 ],
-    [ '__.dgnd_0.GND', 'C1', 2 ],
-    [ '__.supply_0.5V', 'v5v', 1 ],
-    [ '__.supply_0.5V', 'v5v:0', 1 ],
-    [ '__.supply_0.5V', 'R1', 1 ]
-  ]
-
-// Store in an array, so that it is accessible
-// during the test itself.
-const allScripts: [string, ComponentPinNet[]][] = [
-    [script1, script1Expected],
-    [script2, script2Expected],
-    [script3, script3Expected],
-    [script6, script6Expected],
-    [script7, script7Expected],
-    [script8, script8Expected],
-    [script9, script9Expected],
-    [script10, script10Expected],
-];
 
 // This tests that an error is generated at the right position for 
 // overlapping connections
