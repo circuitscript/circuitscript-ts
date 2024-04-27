13c4599edede6f1ba0846cc69ca4ec8219027aba
Weihao <mail@weihao.dev>
2024-04-27 06:41:04 +0200

    1. clear typescript errors

--- src/antlr/CircuitScriptVisitor.ts
+++ src/antlr/CircuitScriptVisitor.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 // Generated from ./src/antlr/CircuitScript.g4 by ANTLR 4.13.1
 
 import {ParseTreeVisitor} from 'antlr4';
